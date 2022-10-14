b0fd606204dac25d51a6f632fa0ed0e108184830
andraric <ricardo.andrade@diconium.com>
2022-10-14 02:07:27 +0100

    Increase timeouts

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,9 @@ module.exports = defineConfig({
   projectId: "zqmuj6",
   viewportWidth: 1920,
   viewportHeight: 1080,
+  defaultCommandTimeout: 20000,
+  pageLoadTimeout: 60000,
+  execTimeout: 60000,
   e2e: {
     supportFile: "cypress/support/e2e.js",
     specPattern: "**/*.feature",
